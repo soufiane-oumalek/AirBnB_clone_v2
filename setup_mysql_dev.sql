eaa025a8895c78d38714331f024499b3af9ad535
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-18 18:39:04 +0000

    creat databases

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- database for hbnb_dev and user and grant
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
