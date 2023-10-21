0140d91593ec0905ba93df52f89c65493dac76c0
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-21 14:33:07 +0000

    web_flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,9 +14,7 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
+    """displays an HTML page with a all State objects in DBStorage.
     """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
@@ -24,9 +22,9 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """remove  SQLAlchemy"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,9 +14,8 @@ app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
+    """displays an HTML page with a list of all states and related cities.
 
-    States/cities are sorted by name.
     """
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
@@ -24,9 +23,9 @@ def cities_by_states():
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """remove the SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
