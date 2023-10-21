4272b06fdb77e989735525638c2a4198e44a9b80
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-21 15:02:14 +0000

    web_flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -13,54 +13,53 @@ Routes:
     /number_odd_or_even/n: Displays an HTML page only if <n> is an integer
     H1 tag: H1 Number: n is even|odd inside the tag BODY
 """
-from flask import Flask, url_for, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
-    """return hello hbhb
-    """
+def hello():
+    """ def doc """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def HBNB():
-    """return HBNB"""
+def hbnb():
+    """ def doc """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text(text):
-    """return text """
-    return "C {}".format(text.replace("_", " "))
+def c(text):
+    """ def doc """
+    return 'c {}'.format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display(text):
-    """display Python  followed by the value of the text"""
-    return "Python {}".format(text.replace("_", " "))
+def python(text):
+    """ def doc """
+    return 'Python {}'.format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def num_display(n):
-    """display n is a number only"""
-    return "{} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def num_html(n):
-    """display HTML is "n" is a number only"""
-    return render_template('5-number.html', name=n)
+def number(n):
+    """ def doc """
+    return '{} is a number'.format(n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def num_html_even_odd(n):
-    """display HTML is "n" is a number only"""
-   return render_template('6-number_odd_or_even.html', name=n)
+def number_odd_or_even(n):
+    """ def doc """
+    if n % 2 == 0:
+        c = 'even'
+    else:
+        c = 'odd'
+    return render_template('6-number_odd_or_even.html', number=c, parity=c)
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
+
